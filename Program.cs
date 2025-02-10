530b5fe579c605bfe43f3a6bb0dda64ca3269026
Ceraptreon <167917044+Ceraptreon@users.noreply.github.com>
2025-02-10 21:00:48 +0300

    Practice OOP

--- Program.cs
+++ Program.cs
@@ -1,82 +1,98 @@
-﻿
-using System;
-class Program
+﻿using System;
+using System.Runtime.InteropServices;
+
+//2
+class BankAccount 
 {
-    static void Main()
+    private int _balance
+    public BankAccount(int balance)
     {
-        
-        // Простой калькулятор:
-        Console.WriteLine("Введите 1 число - ");
-          int a = int.Parse(Console.ReadLine());
+        _balance = balance;
+    }
+    public void deposit(int amount)
+    {
+        Console.WriteLine($"Ваш депозит - {amount}");
+    }
+    public virtual void withdraw(int amount2)
+    {
+        Console.WriteLine($"Вы вывели - {amount2}");
+    }
+    public void GetBalance()
+    {
+        Console.WriteLine($"Ваш счет насчитывает - {_balance}");
+    }
+    class SavingAccount : BankAccount 
+    { 
+        public override void withdraw(int amount2) 
+        {
+            Console.WriteLine("Вы не можете вывести деньги");
+        }
+    }
+    class CreditAccount : BankAccount
+    {
+        private int _credit;
+        public CreditAccount(int credit)
+        {
+            _credit = credit;
+        }
+    }
 
-        Console.WriteLine("Введите 2 число - ");
-          int b = int.Parse(Console.ReadLine());
+}
 
-        Console.WriteLine("Выберите математическое действие (1 - Сложение, 2 - Вычитание, 3 - Умножение, 4 - Деление.) --  ");
-          int c = int.Parse(Console.ReadLine());
 
-        if(c == 1)
-        {
-            Console.WriteLine($"Произведено сложение - {a + b} ");
-        }
-        else if (c == 2)
-        {
-            Console.WriteLine($"Произведено вычитание - {a - b} ");
-        }
-        else if (c == 3)
-        {
-            Console.WriteLine($"Произведено умножение - {a * b} ");
-        }
-        else if (c == 4)
-        {
-            if(b == 0)
-            {
-                Console.WriteLine("Нельзя делить на ноль!!!");
-                return;
-            }
-            Console.WriteLine($"Произведено деление - {a / b} ");
-        }
-        else
-        {
-            Console.WriteLine("Ошибка!!!");
-        }
-        
-        
-        // Игра - Угадай число!
-        Random rnd = new Random();
-        Console.WriteLine("Угадайте число от 1 до 10! --");
-        int num = rnd.Next(1, 11);
-        int PlayerNum = int.Parse(Console.ReadLine());
-        if (PlayerNum > num)
-        {
-            Console.WriteLine("Число Игрока больше загадонного (у вас ещё одна попытка)! Повторно введтие число -- ");
-        }
-        else if (PlayerNum < num)
-        {
-            Console.WriteLine("Число Игрока меньше загадонного (у вас ещё одна попытка)! Повторно введтие число -- ");
-        }
 
-        else if (PlayerNum == num)
-        {
-            Console.WriteLine("Вы угадали!!!");
-        }
-        else
-        {
-            Console.WriteLine("Число Игрока не может быть меньше 1 или больше 10!!!");
-        }
-        int PlayerNum1 = int.Parse(Console.ReadLine());
-        if (PlayerNum1 == num)
-        {
-            Console.WriteLine("Вы угадали со 2 попытки!!!!");
-        }
-        else
-        {
-            Console.WriteLine("Вы проиграли :( ");
-        }
+//3
+interface Discount
+{
+    public void Apply_Discount(int discount);
+    
 
+}
+
+class Product : Discount
+{
+
+    public void Apply_Discount(int discount)
+    {
+        price = discount / 100 * price;
+        Console.WriteLine(price);
+    }
+
+    public string name;
+    public Product(string _name)
+    {
+        name = _name;
+    }
+    public double price;
+    public Product(double _price)
+    {
+        price = _price;
+    }
+
+
+}
+class FoodProduct : Product
+{
+    public double shelf_life;
+    public FoodProduct(double _shelf_life)
+    {
+        shelf_life = _shelf_life;
+    }
+    class Electronics
+    {
+        public double warranty;
+        public FoodProduct(double _warranty)
+        {
+            warranty = _warranty;
+        }
 
     }
 
 
 }
+    
+
+
+
+
 
